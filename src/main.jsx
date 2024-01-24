028551eda1738d820425a08a2b2e87f02fee8665
Igor <igglike@gmail.com>
2024-01-24 16:54:17 +0600

    Добавил Redux и один редьюсер с обработкой двух экшенов

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,36 @@
-import ReactDOM from 'react-dom/client'
-import {BrowserRouter} from 'react-router-dom'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { legacy_createStore as createStore } from "redux";
 
-import './index.css'
-import App from './App'
+import "./index.css";
+import App from "./App";
 
+const defaultState = {
+  counter: 100,
+  user: "Igor"
+};
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// action - действие, которое будет выполнено в store
+// {type: 'INCREMENT' } - действие, объект, у которого обязательное поле type
+// {type: 'DECREMENT' } - объект, у которого обязательное поле type
+const counterReducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { ...state, counter: state.counter + 1 };
+    case "DECREMENT":
+      return { ...state, counter: state.counter - 1 };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counterReducer);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
--- src/pages/CounterPage.jsx
+++ src/pages/CounterPage.jsx
@@ -1,15 +1,28 @@
+import { useSelector, useDispatch } from "react-redux";
 
 const CounterPage = () => {
+  const val = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const onIncrase = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+
   return (
     <div className="w-full flex flex-col justify-center gap-3">
-      <span className='text-2xl col-span-full'>Счетчик</span>
+      <span className="text-2xl col-span-full">Счетчик</span>
       <div className="w-full flex justify-center items-center gap-4">
         <button className="w-8 bg-slate-300 px-2 py-1 rounded-md">-</button>
-        <span>0</span>
-        <button className="w-8 bg-slate-300 px-2 py-1 rounded-md">+</button>
+        <span>{val}</span>
+        <button
+          className="w-8 bg-slate-300 px-2 py-1 rounded-md"
+          onClick={onIncrase}
+        >
+          +
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CounterPage
+export default CounterPage;
